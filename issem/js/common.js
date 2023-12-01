9320b624a7c268c542a6f533a597cca70dcb098e
lopins <lopins@issem.cn>
2023-12-01 22:44:19 +0800

    common.js + global.js = common.js

--- issem/js/common.js
+++ issem/js/common.js
@@ -1,37 +1,331 @@
-/*
-// yewu
-// baidutongji
-var _hmt = _hmt || [];
-(function() {
-	var hm = document.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?8b6b04060f460dd92d7b09305d068717";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-})();
+let oScroll = $(document).scrollTop();
+
+function intLogo(oScroll) {
+    if (oScroll > 0) {
+        $('.header').addClass("header-active");
+    } else {
+        $('.header').removeClass("header-active");
+    }
+}
+intLogo(oScroll);
+// 
+
+$(window).scroll(function(e) {
+    let oScroll = $(document).scrollTop();
+    // console.log(oScroll);
+    intLogo(oScroll);
+
+});
 
 
-// dongsoudi
-(function(){
-	var dsd= document.createElement("script");
-	dsd.src="https://www.dongsoudi.com/analysis?key=dscz34xw0kt2g&ref="+encodeURIComponent(document.referrer);
-	var s = document.getElementsByTagName("script")[0];
-	document.getElementsByTagName("head")[0].appendChild(dsd);
-})();
-*/
+// 移动端
+function clearTouch(el, oStyle) {
+    $(el).css('overflow', oStyle);
+    $(el).on("touchmove", function(e) {
+        e.preventDefault;
+    })
+}
 
-// baidutuisong
-(function(){
-	var bp = document.createElement('script');
-	var curProtocol = window.location.protocol.split(':')[0];
-	if (curProtocol === 'https'){
-		bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-	}
-	else{
-		bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-	}
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(bp, s);
-})();
+$('.navbar').click(function() {
+    $('.burger-bg').removeClass('burger-bg-active');
+    clearTouch("body", "hidden")
+})
+
+
+$('.close-burger').click(function() {
+    $('.burger-bg').addClass('burger-bg-active');
+    clearTouch("body", "auto")
+})
+
+
+
+
+$('.burger-nav-lists>li').click(function(e) {
+    if ($(this).find('.burger-nav-f1').length > 0) {
+        if ($(this).find('.burger-nav-f1').css('display') == 'none') {
+            // 菜单展开
+            $(this).find(".burger-nav-f1").slideDown(200);
+            // 其他菜单收起
+        } else {
+            $(this).find(".burger-nav-f1").slideUp(200);
+            $(this).find(".burger-nav-f1").slideUp(200)
+            $(this).find(".burger-nav-f2").slideUp(200)
+        }
+
+
+    } else {}
+})
+
+$('.burger-nav-f1>li').click(function(e) {
+    e.stopPropagation();
+    if ($(this).find('.burger-nav-f2').length > 0) {
+
+        if ($(this).find('.burger-nav-f2').css('display') == 'none') {
+            $(this).find(".burger-nav-f2").slideDown(200);
+
+        } else {
+            $(this).find(".burger-nav-f2").slideUp(200);
+        }
+
+    } else {
+
+    }
+})
+
+
+var pageCaseBottom = new Swiper(".seo-coug .swiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    loop: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+        nextEl: ".seo-coug .swiper-button-next",
+        prevEl: ".seo-coug .swiper-button-prev",
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        860: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        1200: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 20,
+        },
+        1400: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 20,
+        },
+        1600: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 20,
+        },
+    }
+});
+
+$('.about-core ul.flex > li').mouseover(function() {
+    $(this).addClass('active').siblings('').removeClass('active');
+})
+
+
+$(".footer-li h3").on("click",function(){
+    
+    if($(this).hasClass("footer-li-active")){
+        $(this).removeClass("footer-li-active")
+        $(this).next('ul').hide()
+    }else{
+        $(this).addClass("footer-li-active")
+        $(this).next('ul').show()
+    }
+    
+
+})
+
+var pageCaseBottom = new Swiper(".page-b2b-bottom", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 20,
+        },
+        860: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 5.5,
+            spaceBetween: 20,
+        },
+        1400: {
+            slidesPerView: 6.5,
+            spaceBetween: 20,
+        },
+        1600: {
+            slidesPerView: 7.5,
+            spaceBetween: 20,
+        },
+    }
+});
+
+var pageCaseTop = new Swiper(".page-b2b-top", {
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".page-b2b-top .swiper-button-next",
+        prevEl: ".page-b2b-top .swiper-button-prev",
+    },
+    thumbs: {
+        swiper: pageCaseBottom
+    }
+});
+
+var pageCaseBottom1 = new Swiper(".page-b2c-bottom", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3.5,
+            spaceBetween: 20,
+        },
+        860: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 5.5,
+            spaceBetween: 20,
+        },
+        1400: {
+            slidesPerView: 6.5,
+            spaceBetween: 20,
+        },
+        1600: {
+            slidesPerView: 7.5,
+            spaceBetween: 20,
+        },
+    }
+});
+
+var pageCaseTop1 = new Swiper(".page-b2c-top", {
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".page-b2c-top .swiper-button-next",
+        prevEl: ".page-b2c-top .swiper-button-prev",
+    },
+    thumbs: {
+        swiper: pageCaseBottom1
+    }
+});
+
+var videoSeo = new Swiper(".video-swiper .swiper", {
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".video-swiper .swiper-button-next",
+        prevEl: ".video-swiper .swiper-button-prev",
+    },
+});
+
+var swiperResults = new Swiper('.slideshow-results .swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    autoplay: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 10,
+        depth: 100,
+        modifier: 1,
+        // slideShadows : true,
+    },
+    pagination: {
+        el: '.swiper-pagination-results',
+    },
+});
+
+
+var swiperCase = new Swiper('.xiao-case-swiper .swiper ', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    autoplay: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 150,
+        modifier: 1,
+        slideShadows : true,
+    },
+    pagination: {
+        el: '.xiao-case-swiper .swiper-pagination-case',
+    },
+});
+
+$(".page-case-thead ul li").eq(0).addClass("on");
+$(".page-case-tbody .page-case-tbody-item").eq(0).show();
+$(".page-case-thead ul li").on("click", function() {
+    $(this).addClass("on").siblings().removeClass("on")
+    $(".page-case-tbody .page-case-tbody-item").eq($(this).index()).show().siblings().hide()
+})
+
+
+$(".wechat-service-l .wechat-service-item").eq(0).addClass("active");
+$(".wechat-service-r img").eq(0).show();
+$(".wechat-service-l .wechat-service-item").on("click", function() {
+    $(this).addClass("active").siblings().removeClass("active")
+    $(".wechat-service-r img").eq($(this).index()).show().siblings().hide()
+})
+
+
+new WOW().init();
+// function mycheckWidth() {
+//  let html = [];
+//  let papeWidth = $(window).width();
+//  if ($(".checkWidth")) {
+//      $(".checkWidth").remove()
+//  }
+//  html =
+//      '<style> .checkWidth { position: fixed; top: 0; left: 0; z-page: 999; padding: 5px; background-color: rgba(0, 0, 0, .5); color: #fff; font-size: 16px; } </style><div class="checkWidth">' +
+//      papeWidth + '</div>';
+//  $("body").append(html)
+// }
+// mycheckWidth()
+// $(window).resize(function() {
+//  mycheckWidth()
+// })
 
 (function(){
 	// news 20210708
@@ -155,3 +449,47 @@ var _hmt = _hmt || [];
     })
 })();
 
+/* 百度推送 */
+(function(){
+    var list = document.getElementsByTagName("a");
+    var url = [];
+    var curProtocol = window.location.protocol.split(':')[0];
+    for (var i = 0; i < list.length; ++i) {
+        if (typeof list[i].href !== "undefined" && list[i].href.includes(window.location.hostname)) {
+            var absoluteLink = new URL(list[i].href, window.location.href).href;
+            if (curProtocol === 'https') {
+                url.push('https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif?l=' + encodeURIComponent(absoluteLink));
+            } else {
+                url.push('http://api.share.baidu.com/s.gif?r=' + encodeURIComponent(window.location.href) + '&l=' + encodeURIComponent(absoluteLink));
+            }
+        }
+    }
+    for (var i = 0; i < url.length; ++i) {
+        var t = new Image();
+        t.src = url[i];
+    }
+})(window);
+
+/* 头条推送 */
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?2da07297707878f3f21d377ee213b8de36a10476b25de977823062c7e6a73fae30632485602430134f60bc55ca391050b680e2741bf7233a8f1da9902314a3fa";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window);
+
+/* 百度分析 */
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?913c9fae0cf0b1a059eb7ae4d94f0ef5";
+  var sp = document.getElementsByTagName("script")[0]; 
+  sp.parentNode.insertBefore(hm, sp);
+})(window);
+
+/* Bing分析 */
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "fpy2h27a9n");
